0ed17e3dc607d2115927ae7a1ef59b2db5cbf011
myselfabhi <abhinavverma2024@gmail.com>
2025-01-02 17:40:38 +0530

    fix: news section acc to new UI

--- src/components/dashboard/NewsSection.tsx
+++ src/components/dashboard/NewsSection.tsx
@@ -15,11 +15,11 @@ const NewsItem = ({
         className="w-full lg:w-20 lg:h-16 sm:w-20 sm:h-20 rounded-md object-cover"
       />
 
-      <div className="flex-1">
-        <h3 className="text-sm font-satoshi font-medium text-gray-800 dark:text-gray-200 mb-1">
+      <div className="flex-1 gap-[1px]">
+        <h3 className="text-sm font-satoshi font-bold text-black dark:text-gray-200 mb-1">
           {title}
         </h3>
-        <p className="text-xs text-[#A8A8A8] dark:text-gray-400 line-clamp-2">
+        <p className="text-sm font-medium text-[#555555] dark:text-gray-400 line-clamp-2">
           {description}
         </p>
       </div>
@@ -56,14 +56,14 @@ const NewsSection = () => {
   ]
 
   return (
-    <div className="w-full mx-auto border-[12px] dark:border-[#1E2327] border-[#fffefe] bg-[#fffefe] dark:bg-[#1E2327] rounded-2xl gap-6 px-6 py-6 mb-12">
+    <div className="w-full mx-auto border-[12px] dark:border-[#1E2327] border-[#fffefe] bg-[#fffefe] shadow-extrasoft dark:bg-[#1E2327] rounded-2xl gap-6 px-6 py-6 mb-12">
       {/* Header */}
-      <h2 className="text-base font-satoshi font-medium text-gray-800 dark:text-gray-200 mb-2">
+      <h2 className=" font-satoshi font-medium text-[#262626] dark:text-gray-200 mb-2">
         Recent News & Updates
       </h2>
 
       {/* News Items */}
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="divide-y divide-[#E0E0E0] dark:divide-gray-700">
         {newsData.map((news, index) => (
           <NewsItem
             key={index}
