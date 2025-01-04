b2dab23c9d0c18dd9a22cb274b65aa882ec60816
myselfabhi <abhinavverma2024@gmail.com>
2025-01-04 19:21:16 +0530

    fix: settings offers form section acc to new UI

--- src/components/dashboard/accounts/profile/Offers-form.tsx
+++ src/components/dashboard/accounts/profile/Offers-form.tsx
@@ -1,11 +1,11 @@
 export default function Offers() {
   return (
     <div className="container font-satoshi mx-auto px-4 py-6 border border-[#F4F4F4] dark:border-[#1E2327] rounded-lg">
-      <div className="bg-[#FCFCFC] dark:bg-[#1E2327] rounded-lg shadow-deep p-6">
+      <div className="bg-[#FCFCFC] dark:bg-[#1E2327] rounded-lg shadow-extrasoft p-6">
         {/* Header */}
-        <div className="text-gray-800 dark:text-gray-200 mb-6 space-y-1">
-          <h2 className=" font-medium">Redeem Your Bonus Code</h2>
-          <p className="text-[10px] font-medium text-[#525252] dark:text-gray-400">
+        <div className="  mb-6 space-y-1">
+          <h2 className=" text-black font-bold dark:text-gray-200">Redeem Your Bonus Code</h2>
+          <p className="text-sm font-bold text-[#525252] dark:text-gray-400">
             Have a Bonus Code? Enter it Below to Unlock Your Special Reward!
           </p>
         </div>
@@ -16,7 +16,7 @@ export default function Offers() {
           <div className="w-full">
             <label
               htmlFor="bonus-code"
-              className="block text-xs text-[#131316] dark:text-gray-400"
+              className="block text-sm font-bold text-[#131316] dark:text-gray-400"
             >
               Code<span>*</span>
             </label>
@@ -24,7 +24,7 @@ export default function Offers() {
               type="text"
               id="bonus-code"
               placeholder="ENTER THE CODE HERE"
-              className="mt-2 w-full p-3 font-medium text-xs border border-gray-300 dark:border-gray-700 rounded-md bg-white dark:bg-[#161B1E] text-[#525252] dark:text-gray-200 placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring focus:ring-orange-300 dark:focus:ring-orange-600"
+              className="mt-2 w-full p-3 font-bold text-sm border border-[#EEEEEE] dark:border-gray-700 rounded-md bg-white dark:bg-[#161B1E] text-[#525252] dark:text-gray-200 placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring focus:ring-orange-300 dark:focus:ring-orange-600"
             />
           </div>
 
@@ -32,11 +32,8 @@ export default function Offers() {
           <div className="flex justify-end">
             <button
               type="submit"
-              className="px-10 py-2  rounded-lg font-medium  hover:bg-orange-600 focus:ring-2 focus:ring-orange-400 dark:focus:ring-orange-600"
-              style={{
-                background:
-                  "linear-gradient(286.17deg, #FB9400 0%, #FFAB38 100%)",
-              }}
+              className="px-10 py-2  bg-[#05C067]  text-white rounded-xl font-medium   focus:ring-2 focus:ring-orange-400 dark:focus:ring-orange-600"
+
             >
               Submit
             </button>
